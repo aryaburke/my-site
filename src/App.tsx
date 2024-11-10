4fe3b4b7b83e81437049333bbb3923ca890e6f88
Arya Burke <aryaburke4@gmail.com>
2024-11-10 00:24:17 -0500

    Routing and skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+function Home() {
   return (
     <div className="container">
       <div>
-        Hi — I'm Arya Burke — I live in Brooklyn, NY — Here you can find some of
-        my art.
+        <i>
+          Hi — I'm Arya Burke — I live in Brooklyn, NY — Here you can find some
+          of my art.
+        </i>
       </div>
-      <div>Photography</div>
-      <div>Writing</div>
-      <div>Music</div>
-      <div>Games</div>
-      <div>Tattooing</div>
-      <div>Contact</div>
+      <div>
+        <a href="/photography">Photography</a>
+      </div>
+      <div>
+        <a href="/writing">Writing</a>
+      </div>
+      <div>
+        <a href="/music">Music</a>
+      </div>
+      <div>
+        <a href="/games">Games</a>
+      </div>
+      <div>
+        <a href="/tattooing">Tattooing</a>
+      </div>
+      <div>
+        <a href="/contact">Contact</a>
+      </div>
     </div>
   );
+}
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
