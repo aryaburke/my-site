5472910f0b0ffa161f6287bf4f1b7adfd7909dc0
Arya Burke <aryaburke4@gmail.com>
2024-11-10 00:03:31 -0500

    Extremely basic site, no CSS

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <div>
+        Hi — I'm Arya Burke — I live in Brooklyn, NY — Here you can find some of
+        my art.
+      </div>
+      <div>Photography</div>
+      <div>Writing</div>
+      <div>Music</div>
+      <div>Games</div>
+      <div>Tattooing</div>
+      <div>Contact</div>
+    </div>
+  );
+}
+
+export default App;
