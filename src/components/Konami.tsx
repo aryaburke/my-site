29fb9e3a6f4767e67f6261b4c9dd8b1a37a3d3b6
Arya Burke <aryaburke4@gmail.com>
2024-12-29 16:01:33 -0500

    Song working

--- src/components/Konami.tsx
+++ src/components/Konami.tsx
@@ -1,26 +1,28 @@
 "use client";
-import React, { MouseEventHandler, useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import KonamiTrigger from "konami";
 import $ from "jquery";
-import useSound from "use-sound";
-
 import complicated from "../assets/04-avril_lavigne-complicated.flac";
 
 const EMOGIRL_STR = "emogirl";
 
 export default function Konami() {
-  const [play] = useSound(complicated);
+  const audio = useMemo((): HTMLAudioElement => new Audio(complicated.src), []);
   const [emogirl, setEmogirl] = useState(false);
 
   useEffect(() => {
     const konami = new KonamiTrigger(() => {
       setEmogirl(!EMOGIRL_STR);
       $("body").toggleClass(EMOGIRL_STR);
-      play();
+      if (emogirl) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
     });
     // for debugging: turn pattern to up arrow
     konami.pattern = "38";
   }, []);
 
-  return <button onClick={play as MouseEventHandler}>xxx</button>;
+  return <></>;
 }
