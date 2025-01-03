21aac101bc84f11475ddbff8919aafb1ddec6796
Arya Burke <aryaburke4@gmail.com>
2025-01-03 16:59:04 -0500

    Some achievement debug logging

--- src/helpers/achievements.tsx
+++ src/helpers/achievements.tsx
@@ -88,6 +88,7 @@ export async function unlockAchievement(name: string) {
     return;
   }
   if (getAchievementState(name) !== true) {
+    console.log(getAchievementState(name));
     localStorage.setItem(name, "true");
     localStorage.setItem(STORAGE_KEYS.lastUnlocked, Date.now().toString());
     window.dispatchEvent(new Event("achievementUnlocked"));
