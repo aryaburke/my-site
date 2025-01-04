f1517f4836bc44e10e1a0520cf3fb78140f35a33
Arya Burke <aryaburke4@gmail.com>
2025-01-04 14:16:02 -0500

    hall of fame draft 1

--- src/app/achievements/hall-of-fame/page.tsx
+++ src/app/achievements/hall-of-fame/page.tsx
@@ -1,9 +1,38 @@
+import Image from "next/image";
+import zube from "../../../assets/winnerImages/zube.png";
+
+type Winner = {
+  name: string;
+  time: string;
+  imageSrc: string;
+  quote: string;
+};
+
+const WINNERS: Winner[] = [
+  {
+    name: "zube",
+    time: "WORLD RECORD: 31 seconds",
+    imageSrc: zube.src,
+    quote: "Hate me or love me, you all watched. That's all you could do.",
+  },
+];
+
 export default function HallOfFame() {
   return (
     <div className="text-container">
-      <p>
-        Nobody has hundred percented Arya's room yet... you could be the first!
-      </p>
+      {WINNERS.map((winner) => (
+        <div className="winners">
+          <Image
+            src={winner.imageSrc}
+            alt={winner.name}
+            width={30}
+            height={30}
+          />
+          <p className="winner-name">{winner.name}</p>
+          <p className="winner-flavor">{winner.time}</p>
+          <p className="winner-flavor">{winner.quote}</p>
+        </div>
+      ))}
     </div>
   );
 }
