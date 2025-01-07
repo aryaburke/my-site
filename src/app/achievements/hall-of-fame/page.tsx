947d4a9e77ab678c33d7ee46fdd85622d38686fd
Arya Burke <aryaburke4@gmail.com>
2025-01-06 19:43:23 -0500

    GPU fix, new hall-of-famers

--- src/app/achievements/hall-of-fame/page.tsx
+++ src/app/achievements/hall-of-fame/page.tsx
@@ -1,19 +1,37 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import zube from "../../../assets/winnerImages/zube.png";
+import roarosa from "../../../assets/winnerImages/roarosa.png";
+import jdan from "../../../assets/winnerImages/jdan.png";
 
 type Winner = {
   name: string;
   time: string;
-  imageSrc: string;
   quote: string;
+  image: StaticImageData;
+  width: number;
 };
 
 const WINNERS: Winner[] = [
   {
+    name: "roarosa",
+    time: "WORLD RECORD: 25 seconds",
+    quote: "I am a master of foolhardy plans.",
+    image: roarosa,
+    width: 50,
+  },
+  {
     name: "zube",
-    time: "WORLD RECORD: 31 seconds",
-    imageSrc: zube.src,
+    time: "31 seconds",
     quote: "Hate me or love me, you all watched. That's all you could do.",
+    image: zube,
+    width: 30,
+  },
+  {
+    name: "jdan",
+    time: "3 milliseconds (CHEATER)",
+    quote: "I earned this.",
+    image: jdan,
+    width: 60,
   },
 ];
 
@@ -21,19 +39,23 @@ export default function HallOfFame() {
   return (
     <div className="text-container">
       {WINNERS.map((winner, idx) => (
-        <div className="winner">
-          <Image
-            src={winner.imageSrc}
-            alt={winner.name}
-            width={30}
-            height={30}
-          />
-          <p className="winner-name">
-            #{idx + 1} {winner.name}
-          </p>
-          <p className="winner-flavor">{winner.time}</p>
-          <p className="winner-flavor">{winner.quote}</p>
-        </div>
+        <>
+          <div className="winner" key={`winner-${idx}`}>
+            <Image
+              src={winner.image.src}
+              alt={winner.name}
+              width={winner.width}
+              height={(winner.image.height / winner.image.width) * winner.width}
+            />
+            <p className="winner-name">
+              #{idx + 1} {winner.name}
+            </p>
+            <p className="winner-flavor">{winner.time}</p>
+            <p className="winner-flavor">{winner.quote}</p>
+          </div>
+          <br />
+          <br />
+        </>
       ))}
     </div>
   );
