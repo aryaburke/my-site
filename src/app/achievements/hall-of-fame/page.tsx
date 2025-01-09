ff16a2362b918378b8e9319e4927ad91c5e27ec7
Arya Burke <aryaburke4@gmail.com>
2025-01-08 21:47:05 -0500

    Benkraft + new zube record

--- src/app/achievements/hall-of-fame/page.tsx
+++ src/app/achievements/hall-of-fame/page.tsx
@@ -2,6 +2,7 @@ import Image, { StaticImageData } from "next/image";
 import zube from "../../../assets/winnerImages/zube.png";
 import roarosa from "../../../assets/winnerImages/roarosa.png";
 import jdan from "../../../assets/winnerImages/jdan.png";
+import benkraft from "../../../assets/winnerImages/benkraft.jpg";
 
 type Winner = {
   name: string;
@@ -13,18 +14,25 @@ type Winner = {
 
 const WINNERS: Winner[] = [
   {
+    name: "zube",
+    time: "WORLD RECORD: 12 seconds",
+    quote: "Hate me or love me, you all watched. That's all you could do.",
+    image: zube,
+    width: 30,
+  },
+  {
     name: "roarosa",
-    time: "WORLD RECORD: 25 seconds",
+    time: "25 seconds",
     quote: "I am a master of foolhardy plans.",
     image: roarosa,
     width: 50,
   },
   {
-    name: "zube",
-    time: "31 seconds",
-    quote: "Hate me or love me, you all watched. That's all you could do.",
-    image: zube,
-    width: 30,
+    name: "benkraft",
+    time: "1 day, 45 minutes, 17 seconds ",
+    quote: "can I get a prize for slowest run",
+    image: benkraft,
+    width: 40,
   },
   {
     name: "jdan",
@@ -51,7 +59,7 @@ export default function HallOfFame() {
               #{idx + 1} {winner.name}
             </p>
             <p className="winner-flavor">{winner.time}</p>
-            <p className="winner-flavor">{winner.quote}</p>
+            <p className="winner-flavor">"{winner.quote}"</p>
           </div>
           <br />
           <br />
